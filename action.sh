04125d6da725b11a081b04bc041d104d6bd21713
Niklas Holloh <niklas.holloh@adesso-mobile.de>
2023-09-07 17:31:36 +0200

    main: Fix incorrect directory

--- action.sh
+++ action.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 if [ -z "$ACTION" ]; then
     echo "No action was specified."
     exit 126
 fi
 
-FILE="${ACTION}.sh"
+FILE="${SCRIPT_DIR}/${ACTION}.sh"
 if [ ! -f "$FILE" ]; then
     echo "Action $ACTION does not exist."
     exit 127
